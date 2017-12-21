17f87ad73f2ef1a322e6b82dc0e44763450009f6
Slaven Peles <peles2@llnl.gov>
2017-12-21 15:33:52 -0800

    Suppress redundant output from Generator2 model.

--- ComponentLib/Generator2/Generator2.cpp
+++ ComponentLib/Generator2/Generator2.cpp
@@ -1,25 +1,25 @@
 /*
- * 
+ *
  * Copyright (c) 2017, Lawrence Livermore National Security, LLC.
  * Produced at the Lawrence Livermore National Laboratory.
  * Written by Slaven Peles <peles2@llnl.gov>.
  * LLNL-CODE-718378.
  * All rights reserved.
- * 
- * This file is part of GridKit. For details, see github.com/LLNL/GridKit 
- * Please also read the LICENSE file. 
- * 
- * Redistribution and use in source and binary forms, with or without 
+ *
+ * This file is part of GridKit. For details, see github.com/LLNL/GridKit
+ * Please also read the LICENSE file.
+ *
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * - Redistributions of source code must retain the above copyright notice, 
+ * - Redistributions of source code must retain the above copyright notice,
  *   this list of conditions and the disclaimer below.
  * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the disclaimer (as noted below) in the 
+ *   this list of conditions and the disclaimer (as noted below) in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name of the LLNS/LLNL nor the names of its contributors may 
- *   be used to endorse or promote products derived from this software without 
+ * - Neither the name of the LLNS/LLNL nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
  *   specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
  * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
@@ -31,30 +31,30 @@
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISINGIN ANY 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISINGIN ANY
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * Lawrence Livermore National Laboratory is operated by Lawrence Livermore 
- * National Security, LLC, for the U.S. Department of Energy, National 
+ *
+ * Lawrence Livermore National Laboratory is operated by Lawrence Livermore
+ * National Security, LLC, for the U.S. Department of Energy, National
  * Nuclear Security Administration under Contract DE-AC52-07NA27344.
- * 
- * This document was prepared as an account of work sponsored by an agency 
- * of the United States government. Neither the United States government nor 
- * Lawrence Livermore National Security, LLC, nor any of their employees 
- * makes any warranty, expressed or implied, or assumes any legal liability 
- * or responsibility for the accuracy, completeness, or usefulness of any 
- * information, apparatus, product, or process disclosed, or represents that 
- * its use would not infringe privately owned rights. Reference herein to 
- * any specific commercial product, process, or service by trade name, 
- * trademark, manufacturer, or otherwise does not necessarily constitute or 
- * imply its endorsement, recommendation, or favoring by the United States 
- * government or Lawrence Livermore National Security, LLC. The views and 
- * opinions of authors expressed herein do not necessarily state or reflect 
- * those of the United States government or Lawrence Livermore National 
- * Security, LLC, and shall not be used for advertising or product 
- * endorsement purposes. 
- * 
+ *
+ * This document was prepared as an account of work sponsored by an agency
+ * of the United States government. Neither the United States government nor
+ * Lawrence Livermore National Security, LLC, nor any of their employees
+ * makes any warranty, expressed or implied, or assumes any legal liability
+ * or responsibility for the accuracy, completeness, or usefulness of any
+ * information, apparatus, product, or process disclosed, or represents that
+ * its use would not infringe privately owned rights. Reference herein to
+ * any specific commercial product, process, or service by trade name,
+ * trademark, manufacturer, or otherwise does not necessarily constitute or
+ * imply its endorsement, recommendation, or favoring by the United States
+ * government or Lawrence Livermore National Security, LLC. The views and
+ * opinions of authors expressed herein do not necessarily state or reflect
+ * those of the United States government or Lawrence Livermore National
+ * Security, LLC, and shall not be used for advertising or product
+ * endorsement purposes.
+ *
  */
 
 #include <iostream>
@@ -65,14 +65,14 @@ namespace ModelLib {
 
 /*!
  * @brief Constructor for a simple generator model
- * 
+ *
  * Arguments passed to ModelEvaluatorImpl:
  * - Number of equations = 2
  * - Number of quadratures = 1
  * - Number of optimization parameters = 1
  */
 template <class ScalarT, typename T, typename I>
-Generator2<ScalarT, T, I>::Generator2() 
+Generator2<ScalarT, T, I>::Generator2()
   : ModelEvaluatorImpl<ScalarT, T, I>(2, 1, 1),
     H_(5.0),
     D_(1.2),
@@ -89,7 +89,7 @@ Generator2<ScalarT, T, I>::Generator2()
     // Set model tolerances
     rtol_ = 1.0e-6;
     atol_ = 1.0e-8;
-    
+
     // Set optimization parameter value and bounds
     param_[0] = Pm_;
     param_up_[0] = 1.5;
@@ -113,13 +113,11 @@ int Generator2<ScalarT, T, I>::allocate()
 template <class ScalarT, typename T, typename I>
 int Generator2<ScalarT, T, I>::initialize()
 {
-    std::cout << "Initialize Generator2..." << std::endl;
-    
     y_[0] = asin(param_[0]/Pmax_);  // asin(Pm/Pmax)
     y_[1] = omega_s_;
     yp_[0] = 0.0;
     yp_[1] = 0.0;
-    
+
     return 0;
 }
 
@@ -153,7 +151,7 @@ int Generator2<ScalarT, T, I>::initializeAdjoint()
     yB_[1] = 0.0;
     ypB_[0] = 0.0;
     ypB_[1] = frequencyPenaltyDer(y_[1]);
-    
+
     return 0;
 }
 
@@ -185,7 +183,7 @@ template <class ScalarT, typename T, typename I>
 int Generator2<ScalarT, T, I>::shortCircuit()
 {
     Pmax_ = 0.0;
-    std::cout << "Generator2 short circuited..." << std::endl;
+    // std::cout << "Generator2 short circuited..." << std::endl;
     return 0;
 }
 
@@ -193,7 +191,7 @@ template <class ScalarT, typename T, typename I>
 int Generator2<ScalarT, T, I>::restore()
 {
     Pmax_ = 1.1378/0.545;
-    std::cout << "Generator2 back online...\n";
+    // std::cout << "Generator2 back online...\n";
     return 0;
 }
 
