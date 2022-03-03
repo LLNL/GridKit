b4f6c7f8b710f44fae079a0bd7f41d381a2bdf90
Asher Mancinelli <ashermancinelli@gmail.com>
2022-03-03 08:05:19 +0600

    Use better naming conventions

--- Examples/Enzyme/Standalone/main.cpp
+++ Examples/Enzyme/Standalone/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
-int foo(int x) {
+int square(int x) {
   return x * x;
 }
 
 int __enzyme_autodiff(int(*)(int), ...);
-int dfoo(int x) {
-  return __enzyme_autodiff(foo, x);
+int dsquare(int x) {
+  return __enzyme_autodiff(square, x);
 }
 
 int main() {
   int fail = 0;
-  if (dfoo(5) != 25)
+  if (dsquare(5) != 25)
     fail++;
   return fail;
 }
